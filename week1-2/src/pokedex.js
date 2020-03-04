479d4abaa02d8caaec53a47f1778f94218a90594
juanipullaro <juanipullaro_97@hotmail.com>
2020-03-03 23:23:58 -0300

    add to fav button ready

--- week1-2/src/pokedex.js
+++ week1-2/src/pokedex.js
@@ -1,17 +1,47 @@
 const section = document.querySelector("section");
 let pokemons = [];
 const show_pokemon = (pokemon) =>{
-    let card = '<div class="card" id="'+pokemon.name+'">'+
+    let card;
+    if (localStorage.getItem(pokemon.id)==null){
+        card = '<div class="card" >'+
                 '<div class="img-card">'+
                 '<img src="'+pokemon.image+'"> </div> '+
                 '<div class="body-card">'+
                 '<h3>'+pokemon.name+'</h3>'+
                 '<p>Types: '+pokemon.types+'</p>'+
-                '<button class="btn-home btn-card">Add to  favs</button>'+
+                '<button class="btn-home btn-card" id="'+pokemon.name+'">Add to favs</button>'+
                 '</div></div>';
+    }
+    else{
+        card = '<div class="card" >'+
+                '<div class="img-card">'+
+                '<img src="'+pokemon.image+'"> </div> '+
+                '<div class="body-card">'+
+                '<h3>'+pokemon.name+'</h3>'+
+                '<p>Types: '+pokemon.types+'</p>'+
+                '<button class="btn-home btn-card btn-remove" id="'+pokemon.name+'">Remove</button>'+
+                '</div></div>';
+    }
     const cards = document.querySelector(".poke-cards");
     cards.insertAdjacentHTML('beforeend',card);
 }
+const add_fav = (pokemon)=>{
+        let button = document.querySelector('#'+pokemon.name);
+        button.addEventListener("click",()=>{
+                if (button.className == "btn-home btn-card"){
+                    localStorage.setItem(pokemon.id,JSON.stringify(pokemon));
+                    button.innerHTML = "Remove";
+                    button.classList.add("btn-remove");
+                    //button.innerHTML ='<button class="btn-home btn-card" id="'+pokemon.name+'">Remove</button>';
+                }
+                else{
+                    localStorage.removeItem(pokemon.id,JSON.stringify(pokemon));
+                    button.innerHTML =  "Add to favs";
+                    button.classList.remove("btn-remove");
+                }
+
+})}
+
 const add_pokemons = () =>{
     for (let i = 1; i <150 ; i++){
         fetch('http://pokeapi.co/api/v2/pokemon/'+i)
@@ -27,11 +57,13 @@ const add_pokemons = () =>{
                 }
                 const pokemon = {'name':name,'id':id,'types':typesArray.toString(),'image':img};
                 pokemons.push(pokemon);
-                show_pokemon(pokemon);  
+                show_pokemon(pokemon); 
+                return pokemon;
             })
+            .then((pokemon)=>{add_fav(pokemon);})
             .catch(error => console.error(error));
     }}
-section.addEventListener('load',add_pokemons());
+
 
 const input = document.querySelector(".search-input");
 let search_pokemons= ()=>{
@@ -43,6 +75,7 @@ let search_pokemons= ()=>{
     section.innerHTML="";
     for (let pokemon of pokemons_filter){
         show_pokemon(pokemon);
+        add_fav(pokemon);
 
     }
 };
@@ -54,6 +87,7 @@ let show_all_pokemons = ()=>{
         section.innerHTML="";
         for (let pokemon of pokemons){
             show_pokemon(pokemon);
+            add_fav(pokemon);
         }
     }
 }
@@ -63,3 +97,26 @@ const btn_search = document.querySelector("#search-btn");
 input.addEventListener("input",show_all_pokemons);
 btn_search.addEventListener("click",search_pokemons);
 
+window.onload = add_pokemons();
+
+
+//const btn_addFavs= document.querySelectorAll(".card button");
+/*const add_favs = () =>{
+    console.log("hola");
+    console.log(btn_addFavs);
+
+    btn_addFavs.addEventListener("click",()=>{
+        pokemon_id = btn.parentElement.parentElement.id;
+        pokemon = pokemons.filter(f = (poke)=> {poke.id = pokemon_id});
+        if (btn_addFavs.value == "Add to favs"){
+            localStorage.setItem(pokemon);
+            btn_addFavs.innerHTML = '<button class="btn-home btn-card">Remove</button>';
+        }
+        else{
+            localStorage.removeItem(pokemon);
+            btn_addFavs.innerHTML =  '<button class="btn-home btn-card">Add to  favs</button>';
+        }
+        
+    })
+}
+window.onload=add_favs;*/
