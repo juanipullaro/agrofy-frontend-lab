2e2492acb3963eccb7fa9271f46e02d38dd8a5b6
juanipullaro <juanipullaro_97@hotmail.com>
2020-03-11 15:55:07 -0300

    add myfavs page

--- week3-4/src/pages/MyFavs/MyFavs.js
+++ week3-4/src/pages/MyFavs/MyFavs.js
@@ -1,19 +1,22 @@
 import React, {useState} from 'react';
 import './MyFavs.css';
+import PokemonCard from '../../components/PokemonCard/PokemonCard';
 
 const MyFavs = () => {
   const [myFavs,setMyFavs] = useState(JSON.parse(localStorage.getItem("myFavs")));
   
+  const removeFavs = (pokemon) => {
+    let aux = myFavs.filter((p) =>{return p.id !== pokemon.id});
+    localStorage.setItem("myFavs", JSON.stringify(aux));
+    setMyFavs(aux);
+    
+  }
   return <div className="myfavs-page">
     <h1>My Favs</h1>
     <div className="pokemon-cards">
-    {filteredPokemon.map((p) =>{
-      if(p.is_fav){
+    {myFavs.map((p) =>{
         return <PokemonCard className="btn-card btn-remove" callback={()=>removeFavs(p)} label="Remove" id={p.id} name={p.name} types={p.types} img={p.image}/>
-      }
-      else{
-        return <PokemonCard className="btn-card" callback={()=>addFavs(p)} label="Add Favs" id={p.id} name={p.name} types={p.types} img={p.image}/>
-      }})}
+     })}
     </div>
   </div>
 };
